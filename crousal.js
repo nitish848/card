4d5e46792e4972412fe663040f04ba5ac049874b
nitish848 <64553426+nitish848@users.noreply.github.com>
2021-05-25 00:00:21 +0530

    Add files via upload

---
+++ crousal.js
@@ -0,0 +1,45 @@
+const state = {};
+const carouselList = document.querySelector('.carousel__list');
+const carouselItems = document.querySelectorAll('.carousel__item');
+const elems = Array.from(carouselItems);
+
+carouselList.addEventListener('click', function (event) {
+  var newActive = event.target;
+  var isItem = newActive.closest('.carousel__item');
+
+  if (!isItem || newActive.classList.contains('carousel__item_active')) {
+    return;
+  };
+  
+  update(newActive);
+});
+
+const update = function(newActive) {
+  const newActivePos = newActive.dataset.pos;
+
+  const current = elems.find((elem) => elem.dataset.pos == 0);
+  const prev = elems.find((elem) => elem.dataset.pos == -1);
+  const next = elems.find((elem) => elem.dataset.pos == 1);
+  const second = elems.find((elem) => elem.dataset.pos == -2);
+  const secondlast = elems.find((elem) => elem.dataset.pos == 2);
+  const first = elems.find((elem) => elem.dataset.pos == -3);
+  const last = elems.find((elem) => elem.dataset.pos == 3);
+  
+  current.classList.remove('carousel__item_active');
+  
+  [current, prev, next,second, first,secondlast, last].forEach(item => {
+    var itemPos = item.dataset.pos;
+
+    item.dataset.pos = getPos(itemPos, newActivePos)
+  });
+};
+
+const getPos = function (current, active) {
+  const diff = current - active;
+
+  if (Math.abs(current - active) > 3) {
+    return -current
+  }
+
+  return diff;
+}
